a2837f0899b490f399ba7389d67448d5ae2c0d2d
webpassionstar <passiondev5555@gmail.com>
2021-12-20 04:02:02 -0800

    FEAT: add header and footer panel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Header from "../layouts/header";
+import Footer from "../layouts/footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
